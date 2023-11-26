369203987deab31e9a18c1eb9ad43341efb12b2e
Adnan <adnanbenyadiop09@gmail.com>
2023-11-26 18:57:46 +0100

    Commit Incial

---
+++ HolaMundo.js
@@ -0,0 +1,12 @@
+String ("HolaMundo");
+console.WriteLine (String
+);
+[
+    {
+        "type": "chrome",
+        "request": "launch",
+        "name": "Launch Chrome against localhost",
+        "url": "http://localhost:8080",
+        "webRoot": "${workspaceFolder}"
+    }
+]
